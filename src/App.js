6e7886c87e67290c83facb5a4462459e32f88e53
Zacharovanii <tuzudavid7@gmail.com>
2024-10-14 12:59:32 +0300

    loader and custom hook

--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./components/hooks/usePosts";
 import PostService from "./components/API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./components/hooks/useFetching";
 
 function App() {
 	const [posts, setPosts] = useState([])
 	const [filter, setFilter] = useState({ sort: '', query: '' })
 	const [modal, setModal] = useState(false)
+	const [totalCount, setTotalCount] = useState(0)
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+		const response = await PostService.getAll()
+		setPosts(response.data)
+		setTotalCount(response.headers['x-total-count'])
+	})
 
 	useEffect(() => {
 		fetchPosts()
@@ -21,11 +30,6 @@ function App() {
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost])
 		setModal(false)
-	}
-
-	async function fetchPosts() {
-		const posts = await PostService.getAll()
-		setPosts(posts)
 	}
 
 	const removePost = (post) => {
@@ -45,11 +49,19 @@ function App() {
 				filter={filter}
 				setFilter={setFilter}
 			/>
-			<PostList
-				remove={removePost}
-				posts={sortedAndSearchedPosts}
-				title='Список постов 1'
-			/>
+			{postError
+				? <h1>Some Error: {postError}</h1>
+				: ''
+			}
+			{isPostsLoading
+				? <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }} ><Loader /></div>
+				: <PostList
+					remove={removePost}
+					posts={sortedAndSearchedPosts}
+					title='Список постов 1'
+				/>
+			}
+
 		</div>
 	);
 }
